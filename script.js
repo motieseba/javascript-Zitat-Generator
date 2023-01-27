1ef93fe5a9d7c32b157ecb491bebe89942a43867
Seba Motie <58521630+motieseba@users.noreply.github.com>
2023-01-27 16:22:55 +0100

    all ready

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const loader = document.getElementById("loader");
 const quateContainer = document.getElementById("quote-container");
+const copyBtn = document.getElementById("copy");
+
+
+
+
+
 
 function loading() {
     loader.hidden = false;
@@ -15,26 +21,34 @@ function complete() {
 }
 //get quate from api
 
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2626166747mshcf49d082d785611p1cb8d5jsne81809b35591',
+        'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+    }
+};
+
+
 async function getQuote(){
     loading();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response= await fetch(proxyUrl + apiUrl);
+        const response= await fetch('https://quotes15.p.rapidapi.com/quotes/random/?language_code=de', options);
+        if (!response.ok) {
+            throw new Error(`Getting error: ${response}`);
+        }
         const data = await response.json(); 
-        document.getElementById("quote").innerText = data.quoteText;
-		if (data.quoteAuthor=== '') {
+        document.getElementById("quote").innerText = data.content;
+		if (data.originator.name=== '') {
             document.getElementById("author").innerText = 'Unkonown';
         } else {
-            document.getElementById("author").innerText = data.quoteAuthor;
-        }
-        if (data.quoteText.length > 120) {
+            document.getElementById("author").innerText = data.originator.name;}
+        if (data.content.length > 120) {
             document.getElementById("quote").classList.add('long-quote');
-        } else {
+        } else {                
             document.getElementById("quote").classList.remove('long-quote');
-            
         }
-        document.getElementById("quote").innerText = data.quoteText;
+        document.getElementById("quote").innerText = data.content;
         complete();
     } catch (error) {
         getQuote(); 
@@ -48,14 +62,12 @@ function tweetquote() {
     const twitterUrl = 'https://twitter.com/intent/tweet?text='+quote+' - '+author;
     window.open(twitterUrl, '_blank');
 }
-function setUrl() {
-    document.getElementById("fb-share").setAttribute( 'data-href', document.URL);
-}
 
 // event listner
-//document.getElementById("facebook").addEventListener('click', setUrl);
+
 document.getElementById("new-quote").addEventListener('click', getQuote);
 document.getElementById("twitter").addEventListener('click', tweetquote);
+copyBtn.addEventListener("click", ()=>{navigator.clipboard.writeText(quoteText.innerText);});
 //On load
 document.getElementById("fb-share").setAttribute( 'data-href', document.URL);
 getQuote();
